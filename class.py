52b534f35cc380d8f473b53bfb266753b774baf7
creepnaro <101armin@gmail.com>
2024-04-09 07:56:19 -0400

    random

--- class.py
+++ class.py
@@ -1,35 +1,26 @@
 import math
+import numpy as np
 
 class playerInfo():
     
-    def __init__(self,rscid,tier,gp,gw,gl,winp,shotp,PPG,GPG,APG,SvPG,ShPG,PAPG,GAPG,AAPG,SvAPG,ShAPG,OPPG,OGPG,OAPG,OSvPG,OShPG,OPAPG,OGAPG,OAAPG,OSvAPG,OShAPG):
+    def __init__(self,rscid):
         self.rscid = rscid
+        self.gp = 0
+
+    def newData(self,tier,gp,gw,gl,winp,shots,ppts):
         self.tier = tier
         self.gp = gp
         self.gw = gw
         self.gl = gl
-        self.winp = winp
-        self.shotp = shotp
-        self.ppg = PPG
-        self.gpg = GPG
-        self.apg = APG
-        self.svpg = SvPG
-        self.shpg = ShPG
-        self.papg = PAPG
-        self.gapg = GAPG
-        self.aapg = AAPG
-        self.svapg = SvAPG
-        self.shapg = ShAPG
-        self.oppg = OPPG
-        self.ogpg = OGPG
-        self.oapg = OAPG
-        self.osvpg = OSvPG
-        self.oshpg = OShPG
-        self.opapg = OPAPG
-        self.ogapg = OGAPG
-        self.oaapg = OAAPG
-        self.osvapg = OSvAPG
-        self.oshapg = OShAPG
+        self.winp = self.gw/self.gw
+        self.shots = shots
+        self.points = ppts
+
+    def appendData(self,gp,gw,gl,winp,shotp,PPG,GPG,APG,SvPG,ShPG,PAPG,GAPG,AAPG,SvAPG,ShAPG,OPPG,OGPG,OAPG,OSvPG,OShPG,OPAPG,OGAPG,OAAPG,OSvAPG,OShAPG):
+        if self.gp != 0:
+            self.newData(gp,gw,gl,winp,shotp,PPG,GPG,APG,SvPG,ShPG,PAPG,GAPG,AAPG,SvAPG,ShAPG,OPPG,OGPG,OAPG,OSvPG,OShPG,OPAPG,OGAPG,OAAPG,OSvAPG,OShAPG)
+        else:
+            
 
     def forIndex(self):
         self.pfi = self.ppg/self.opapg
@@ -44,3 +35,7 @@ class playerInfo():
         self.aai = self.aapg/self.oapg
         self.svai = self.svapg/self.osvpg
         self.shai = self.shapg/self.oshpg
+
+
+RSCID = {}
+
