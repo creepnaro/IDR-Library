8c62021374585e5bab0fdb1a5b8460a8dbfc8ba3
creepnaro <101armin@gmail.com>
2024-04-09 23:54:46 -0400

    .

--- class.py
+++ class.py
@@ -7,19 +7,36 @@ class playerInfo():
         self.rscid = rscid
         self.gp = 0
 
-    def newData(self,tier,gp,gw,gl,winp,shots,ppts):
+    def newData(self,tier,gp,gw,gl,winp,shots,ppts,ff = False,PAPG,GAPG,AAPG,SvAPG,ShAPG,OPPG,OGPG,OAPG,OSvPG,OShPG,OPAPG,OGAPG,OAAPG,OSvAPG,OShAPG):
         self.tier = tier
         self.gp = gp
         self.gw = gw
         self.gl = gl
         self.winp = self.gw/self.gw
-        self.shots = shots
-        self.points = ppts
+        self.spg = [shots]
+        self.ppg = [ppts]
+        self.papg = [PAPG]
+        self.gapg = [GAPG]
+        self.aapg = [AAPG]
+        self.svapg = [SvAPG]
+        self.shapg = [ShAPG]
+        self.oppg = [OPPG]
+        self.ogpg = [OGPG]
+        self.osvpg = [OSvPG]
+        self.oshpg = [OShPG]
+        self.opapg = [OPAPG]
+        self.ogapg = [OGAPG]
+        self.osvapg = [OSvAPG]
+        self.oshapg = [OShAPG]
+
+
 
     def appendData(self,gp,gw,gl,winp,shotp,PPG,GPG,APG,SvPG,ShPG,PAPG,GAPG,AAPG,SvAPG,ShAPG,OPPG,OGPG,OAPG,OSvPG,OShPG,OPAPG,OGAPG,OAAPG,OSvAPG,OShAPG):
         if self.gp != 0:
             self.newData(gp,gw,gl,winp,shotp,PPG,GPG,APG,SvPG,ShPG,PAPG,GAPG,AAPG,SvAPG,ShAPG,OPPG,OGPG,OAPG,OSvPG,OShPG,OPAPG,OGAPG,OAAPG,OSvAPG,OShAPG)
         else:
+            print('HI')
+            
             
 
     def forIndex(self):
